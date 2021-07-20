d65c3e338c52001ae6410fdec97a5ece3b9562f1
Harumaro <ID+username@users.noreply.github.com>
2021-07-20 09:55:52 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: './typescript/script.ts',
+
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader',
+            },
+        ],
+    },
+    // import 文で .ts ファイルを解決するため
+    // これを定義しないと import 文で拡張子を書く必要が生まれる。
+    // フロントエンドの開発では拡張子を省略することが多いので、
+    // 記載したほうがトラブルに巻き込まれにくい。
+    resolve: {
+        // 拡張子を配列で指定
+        extensions: [
+            '.ts', '.js',
+        ],
+    },
+
+    output: {
+        // 出力するファイル名
+        filename: 'script.js',
+        // 出力先のパス（v2系以降は絶対パスを指定する必要がある）
+        path: path.join(__dirname, 'build')
+    }
+};
